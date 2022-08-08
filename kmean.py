04b1a40aba82a3f6c86e13400e92c02671e5b881
Jai Singh <jai.singh3705@gmail.com>
2022-08-08 16:47:27 -0500

    reasonable cluster

---
+++ kmean.py
@@ -0,0 +1,94 @@
+import pandas as pd
+import numpy as np;
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+from sklearn.preprocessing import MinMaxScaler
+# import seaborn as sns
+import sklearn.cluster as cluster
+import sklearn.metrics as metrics
+
+TRIAL = 1
+
+filename = 'patientavgs.csv'
+pddata = pd.DataFrame(pd.read_csv(filename, header=0))
+dropcols = ['ID', 'height', 'stress',
+       'oversleeping', 'lack_of_sleep', 'fatigue',
+       'menstrual_cycle', 'weather_temp_change',
+       'noise', 'specific_smell',
+       'irregular_meals', 
+       'other_triggers1', 'other_triggers2',
+       'help_sleep', 'help_rest', 'help_massage_or_stretching',
+       'help_exercise', 'help_other',  'gourmet', 'incapacitated_degree',  'throbbing1', 'no_housework', 
+       'headache_freq', 'duration_min',  'pain_intensity', 'left_side', 'right_side', 'middle', 'both_sides','sensitivity_to_light', 'light_noise_sensitivity', 'unilateral', 'sensitive_to_sound', 'worse_with_movement', 'nausea_vomiting', 'around_the_eyes',  'weight' ,'back_of_the_neck', 'sex','age', ]
+# 'bmi', 'tight_headache', 'throbbing2', 'dull_heavy_headache', 'throw_up',
+
+ids = pddata['ID']
+pddata = pddata.drop(dropcols, axis=1)
+# pddata = pddata.drop(pddata[pddata['sex']==0].index)
+# pddata = pddata.drop(['sex', 'age'], axis=1)
+pddata.fillna(0, inplace=True)
+print(pddata.index)
+
+# print(pddata.sum(['left_side', 'right_side', 'middle',
+    #    'both_sides', 'around_the_eyes', 'back_of_the_neck', 'unilateral',
+    #    'tight_headache', 'throbbing2', 'dull_heavy_headache'], axis=1))
+
+# print(pddata.columns)
+scaler = MinMaxScaler()
+scaled = scaler.fit_transform(pddata)
+pddata_scaled = pd.DataFrame(scaled, columns=pddata.columns)
+# print(pddata_scaled.sum(axis=0))
+
+K = range(2, 50)
+wss = []
+silhouette_score = []
+
+print(pddata.columns)
+
+###########################################################
+###########################################################
+for k in K:
+    km = KMeans(n_clusters=k, init='k-means++')
+    km=km.fit(pddata_scaled)
+    wss_iter = km.inertia_
+    wss.append(wss_iter)
+    labels=cluster.KMeans(n_clusters=k,random_state=200).fit(pddata_scaled).labels_
+    silhouttescore = metrics.silhouette_score(pddata_scaled,labels,metric="euclidean",sample_size=1000,random_state=0)
+    print ("Silhouette score for k(clusters) = "+str(k)+" is "
+    +str(silhouttescore))
+    silhouette_score.append(silhouttescore)
+
+sil_data = np.array(silhouette_score)
+print('optimal cluster: ', 2+np.argmin(-1*sil_data))
+
+print(np)
+
+plt.xlabel('K')
+plt.ylabel('Within-Cluster-Sum of Squared Errors (WSS)')
+plt.plot(K,wss)
+plt.show()
+###########################################################
+###########################################################
+    
+###########################################################
+###########################################################
+# 'age', 'bmi', 'tight_headache', 'throbbing2', 'dull_heavy_headache', 
+
+# k = 5
+# km = KMeans(n_clusters=k, init='k-means++')
+# result = km.fit_predict(pddata_scaled)
+# pddata['cluster'] = km.labels_
+# # print(km.labels_)
+# all_cols = ['cluster'] + list(pddata.columns)
+# results = pd.DataFrame(columns=all_cols, dtype='float')
+# print(pddata.columns)
+# pddata.to_csv('kmean_results/clustering_result.csv')
+
+# plt.scatter(pddata['bmi'], pddata['age'], c=km.labels_, s=50, alpha=0.5)
+# plt.show()
+###########################################################
+###########################################################
+
+# results['cluster'] = km.labels_
+# for col in pddata.columns:
+#     results[col] = pddata[col]
--- processdata.py
+++ processdata.py
@@ -1,21 +1,22 @@
+from operator import index
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-data = pd.DataFrame(pd.read_csv('patient_info.csv', header=0))
+patient_info = pd.DataFrame(pd.read_csv('patient_info.csv', header=0))
 
-data['sex'].replace(['F','M'],[0,1], inplace=True)
+patient_info['sex'].replace(['F','M'],[0,1], inplace=True)
 
-patients = data['patient']
-data = data.drop(['patient'], axis = 1)
-data.index= patients
+patients = patient_info['patient']
+patient_info = patient_info.drop(['patient'], axis = 1)
+patient_info.index= patients
 
 for p in patients:
-    if(pd.isna(data['weight'].loc[p]) or pd.isna(data['height'].loc[p]) or pd.isna(data['bmi'].loc[p])):
-        data = data.drop([p])     
+    if(pd.isna(patient_info['weight'].loc[p]) or pd.isna(patient_info['height'].loc[p]) or pd.isna(patient_info['bmi'].loc[p])):
+        patient_info = patient_info.drop([p])     
 
 
-patient_data = ['sex', 'age', 'height', 'weight', 'bmi', 'headache_freq']
+patient_info_cols = ['sex', 'age', 'height', 'weight', 'bmi', 'headache_freq']
 migraine_identifiers = [
     'duration_min',
     'pain_intensity',
@@ -77,5 +78,73 @@ helpful_actions = [
     'help_other',
 ]
 
-print(data)
-# print(data.to_string())
+
+allfields = patient_info_cols + migraine_identifiers + migraine_triggers + helpful_actions
+
+migraine_data = pd.DataFrame(pd.read_csv('migraine_data.csv',header=0))
+idx_delete = migraine_data.index[migraine_data['duration_min']>3000]
+print(migraine_data.shape)
+migraine_data = migraine_data.drop(idx_delete)
+
+print(migraine_data.shape)
+uids = migraine_data['ID'].unique()
+
+patient_averages = pd.DataFrame(columns=allfields, dtype='float')
+
+for id in uids:
+    try:
+        patient_data_i = patient_info.loc[id]
+
+        patient_i = migraine_data.loc[migraine_data['ID']==id]
+        
+        pda = patient_i[patient_i.columns.intersection(allfields)]
+        means = pda.mean()
+        patient_averages.loc[id] = means
+        patient_averages['sex'].loc[id] = patient_info['sex'].loc[id]
+        patient_averages['age'].loc[id] = patient_info['age'].loc[id]
+        patient_averages['height'].loc[id] = patient_info['height'].loc[id]
+        patient_averages['weight'].loc[id] = patient_info['weight'].loc[id]
+        patient_averages['bmi'].loc[id] = patient_info['bmi'].loc[id]
+        patient_averages['headache_freq'].loc[id] = patient_info['headache_freq'].loc[id]
+
+    except:
+        print('id not found', id)
+
+# for c in patient_averages.columns:
+#     if c!='height' and c!='bmi' and c!='headache_freq' and c!='pain_intensity' and c !='duration_min' and c!='weight' and  c!='incapacitated_degree' and c!='age':
+#         for idx in patient_averages.index:
+#             if patient_averages[c].loc[idx]<=0.33:
+#                 patient_averages[c].loc[idx]=0
+#             elif patient_averages[c].loc[idx]<0.67:
+#                  patient_averages[c].loc[idx]=1
+#             else:
+#                 patient_averages[c].loc[idx]=2
+            
+
+        # patient_averages[c].loc[patient_averages[c]<=0.3]=0
+        # patient_averages[c].loc[patient_averages[c]>0.3 & patient_averages[c]<=0.67]=1
+        # patient_averages[c].loc[patient_averages[c]>0.67]=2
+
+patient_averages = patient_averages.drop(['exercise',
+'no_exercise',
+'ovulation',
+'emotion',
+'excessive_sunlight',
+'improper_lighting',
+'drinking',
+'overeating',
+'caffeine',
+'smoking',
+'cheese_chocolate',
+'travel',
+'Missed_school_work',
+'Work_decreased_less_than_half',
+'Housework_reduced_to_less_than_half',
+'Sensitivity_to_odors',
+
+], axis=1)
+
+print(patient_averages.columns)
+patient_averages.to_csv('patientavgs.csv')
+
+# print(patient_averages)
