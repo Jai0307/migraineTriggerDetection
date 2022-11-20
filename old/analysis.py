d6152e2eeb5e830de6c18776b3d1a2a979719c8e
Jai Singh <jai.singh3705@gmail.com>
2022-11-20 09:16:52 -0600

    final

--- old/analysis.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-
-patient_avgs = pd.DataFrame(pd.read_csv('patientavgs.csv', header=0))
-
-plt.scatter(patient_avgs['nausea_vomiting'], patient_avgs['pain_intensity'])
-plt.show()
